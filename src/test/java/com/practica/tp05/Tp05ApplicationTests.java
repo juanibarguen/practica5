c1127089ff015a783d7f53da5ea5b75d1bdcddf3
juanibarguen <juanibarguen159@gmail.com>
2022-05-16 20:37:03 -0300

    Practica 5 en GitHub

---
+++ src/test/java/com/practica/tp05/Tp05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.tp05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
